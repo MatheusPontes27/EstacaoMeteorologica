acdbb38076891d73e6792fc1bb3f59b6a7c02f39
MatheusPontes27 <matheusemmanuel71@gmail.com>
2024-09-01 20:40:29 -0300

    UPDATE MASTER

---
+++ public/script.js
@@ -0,0 +1,37 @@
+
+
+// script.js
+function formatDate(timestamp) {
+    const date = new Date(timestamp);
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    return `Data: ${day}/${month}/${year} , Hora: ${hours}:${minutes}`;
+}
+
+function fetchData() {
+    fetch('/api/data')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.length > 0) {
+                const latestData = data[data.length - 1]; // Obtém o último dado
+                document.getElementById('timestamp').innerText = formatDate(latestData.timestamp);
+                document.getElementById('temperature').innerText = `Temperatura: ${latestData.temperature} °C`;
+                document.getElementById('humidity').innerText = `Umidade: ${latestData.humidity} %`;
+            }
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
+}
+
+// Atualizar dados a cada 5 segundos
+setInterval(fetchData, 5000);
+fetchData(); // Fetch imediatamente
